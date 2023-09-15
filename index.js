c50ff5b4296eed93e5b1e4ed05f03f6aabf4dc52
Mamunur Rashid <devmamunur1@gmail.com>
2023-09-15 22:52:14 +0600

    made auth and articles route and change server port

--- index.js
+++ index.js
@@ -9,12 +9,54 @@ const app = express();
 app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
-app.get('/health', (req, res) => {
+app.get('/api/v1/articles', (req, res) => {
     res.status(200).json({
-        health : 'OK',
+        path : '/articles',
+        method : 'get'
+    });
+});
+app.get('/api/v1/articles/:id', (req, res) => {
+    res.status(200).json({
+        path : '/articles/{id}',
+        method : 'get'
+    });
+});
+app.put('/api/v1/articles/:id', (req, res) => {
+    res.status(200).json({
+        path : '/articles/{id}',
+        method : 'put'
+    });
+});
+app.patch('/api/v1/articles/:id', (req, res) => {
+    res.status(200).json({
+        path : '/articles/{id}',
+        method : 'patch'
+    });
+});
+app.delete('/api/v1/articles/:id', (req, res) => {
+    res.status(200).json({
+        path : '/articles/{id}',
+        method : 'delete'
     });
 });
 
-app.listen(4000, () => {
-    console.log('Server is listening on port 4000');
+app.post('/api/v1/auth/signup', (req, res) => {
+    res.status(200).json({
+        path : '/auth/signup',
+        method : 'post'
+    });
+});
+app.post('/api/v1/auth/signin', (req, res) => {
+    res.status(200).json({
+        path : '/auth/signin',
+        method : 'post'
+    });
+});
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000');
 })
